2ab5277f026d3de66138987e0a6b6780f5a53102
Cade Chaney <cadechaney@live.com>
2023-07-24 20:48:08 -0500

    Add header skeleton component being rendered in App

--- the-due-dork-times/src/components/App/App.js
+++ the-due-dork-times/src/components/App/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Header from './Header/Header'
+
 
 function App() {
   return (
-    <p>The due Dork Times</p>
+    <>
+      <p>The due Dork Times</p>
+      <Header />
+    </>
   );
 }
 
