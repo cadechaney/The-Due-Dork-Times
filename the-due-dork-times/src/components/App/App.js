a5a9063ad711bfa3278487ac0ef93020071040e7
Cade Chaney <cadechaney@live.com>
2023-07-25 17:58:50 -0500

    Add mock data to state in app

--- the-due-dork-times/src/components/App/App.js
+++ the-due-dork-times/src/components/App/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import './App.css';
 import Header from './Header/Header'
-
+import MockData from '../../MockData/mockData.json'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      articles: []
+      articles: [MockData]
     }
   }
 
   render() {
+    console.log(this.state.articles[0].articles)
     return (
       <>
         <Header />
