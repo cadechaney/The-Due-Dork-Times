2a7e6e18203857e47398179a020bf22749626826
Cade Chaney <cadechaney@live.com>
2023-07-26 14:13:31 -0500

    Add basic search box query state

--- the-due-dork-times/src/components/App/Header/Header.js
+++ the-due-dork-times/src/components/App/Header/Header.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import searchIcon from '../../../assets/searchIcon.png'
 
 function Header() {
+
+  const [query, setQuery] = useState('')
+  const handleSubmit = (query, event) => {
+    event.preventDefault()
+    // Function from App
+    clearInputs()
+  }
+
+  const clearInputs = () => {
+    setQuery('')
+  }
+
   return(
     <header>
       <section className='search-section'>
         <img src={searchIcon} className='search-icon' alt='search icon'></img>
-        <input type='text' className='search-box'></input>
+        <input 
+          type='text' 
+          className='search-box'
+          placeholder='Search for Articles'
+          name='query'
+          value={query}
+        ></input>
       </section>
       <section className='header-title'>
         <h1>The Due Dork Times: Smart News for All</h1>
