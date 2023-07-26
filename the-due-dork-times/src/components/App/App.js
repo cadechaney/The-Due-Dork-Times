0b35e9947e7fe1c89b1617a5861a4f3a88bf8c82
Cade Chaney <cadechaney@live.com>
2023-07-26 09:53:21 -0500

    Add new path to single article

--- the-due-dork-times/src/components/App/App.js
+++ the-due-dork-times/src/components/App/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     return (
       <>
         <Routes>
-          <Route path='/:id' element={<ArticleDetails />} />
+          <Route path='/article/:id' element={<ArticleDetails />} />
           <Route path='/' element={<ArticlesList articles={this.state.articles} viewArticle={this.viewArticle} />}/>
         </Routes>
       </>
