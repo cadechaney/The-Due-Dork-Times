197274c2fa95132516ce817cf6ef40b9cf07865e
Cade Chaney <cadechaney@live.com>
2023-07-25 19:46:43 -0500

    Add map iterator to list and pass props to article

--- the-due-dork-times/src/components/App/ArticlesList/ArticlesList.js
+++ the-due-dork-times/src/components/App/ArticlesList/ArticlesList.js
@@ -2,14 +2,19 @@ import React from 'react'
 import './ArticlesList.css'
 import Article from '../../Article/Article'
 
-function ArticlesList() {
+function ArticlesList({ articles }) {
 
-  
+  console.log('articleList', articles)
+
+  const articlesList = articles.map((article, index) => (
+    <Article key={index} author={article.author} />
+  ))
+
   return(
-    <>
-      <p>Articles List</p>
-      <Article />
-    </>
+      <div>
+        {articlesList}
+      </div>
+   
   )
 }
 
